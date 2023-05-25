a93576600a88409c27c5c3a85e059f83935e671a
Rafael Borba <36474347+rafaelborbacs@users.noreply.github.com>
2023-05-25 20:54:29 -0300

    feature

--- apimirror.js
+++ apimirror.js
@@ -1,7 +1,9 @@
 const killPort = require('kill-port')
 const express = require('express')
-const { notifyWS } = require('./wsmirror.js')
 const { getMirrorFiles } = require('./scpmirror.js')
+
+const arrived = []
+const processing = []
 
 const startAPI = async () => {
     killPort(process.env.apiport)
@@ -17,19 +19,33 @@ const startAPI = async () => {
             next()
         })
         api.get('/status', async (req, res) => res.json({msg:'ok'}))
-        api.post('/mirrorfiles', async (req, res) => {
-            req.uuid = Math.random().toString(36).substring(2, 9)
-            getMirrorFiles(req, res)
-        })
-        api.all('*', async (req, res) => {
-            req.uuid = Math.random().toString(36).substring(2, 9)
-            req.callback = (data) => {
-                if(data)
-                    res.status(data.status).json(data.body)
-                else
-                    res.status(404).send({msg: 'WS error: node not found'})
+        api.post('/mirrorfiles', getMirrorFiles)
+        api.get('/get', (req, res) => {
+            const originalReq = arrived.find(r => r.headers.authentication === req.headers.authentication && r.headers.name === req.headers.name)
+            if(originalReq){
+                arrived.splice(arrived.indexOf(originalReq), 1)
+                processing.push(originalReq)
+                res.json(originalReq)
             }
-            notifyWS(req)
+            else res.json(false)
+        })
+        api.put('/put', (req, res) => {
+            const { authentication, name, uuid } = req.headers
+            if(!authentication || !name || !uuid)
+                res.status(400).json({msg: 'authentication and uuid required'})
+            const originalReq = processing.find(r => r.authentication === authentication && r.name === name && r.uuid === uuid)
+            if(originalReq){
+                processing.splice(processing.indexOf(originalReq), 1)
+                const { status, body } = req.body
+                res.status(status).json(body)
+            }
+            else res.status(404).json({msg: 'not found'})
+        })
+        api.all('*', (req, res) => {
+            if(!req || !req.headers || !req.headers.authentication || !req.headers.name)
+                res.status(400).json({msg: 'authentication required'})
+            req.headers.uuid = Math.random().toString(36).substring(2, 9)
+            arrived.push(req)
         })
         api.listen(process.env.apiport, () => console.log(`API Mirror listening on port ${process.env.apiport}`))
     })
--- mirror.js
+++ mirror.js
@@ -1,8 +1,6 @@
 require('dotenv').config()
 const { startSCP } = require('./scpmirror.js')
 const { startAPI } = require('./apimirror.js')
-const { startWSServer } = require('./wsmirror.js')
 
 startSCP()
 startAPI()
-startWSServer()
