084bf73cacb94b478533d55e367698b9499cb63a
Rafael Borba <36474347+rafaelborbacs@users.noreply.github.com>
2023-07-08 09:23:33 -0300

    Feature

--- mirror.js
+++ mirror.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
+const { exec } = require('child_process')
 const { startSCP } = require('./scpmirror.js')
 const { startAPI } = require('./apimirror.js')
 
 startSCP()
 startAPI()
+exec('curl ifconfig.me', (err, stdout, stderr) => {
+    if(stdout) console.log(`Mirror IP: ${stdout}`)
+})
